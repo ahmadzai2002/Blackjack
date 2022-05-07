be49faba06636c326fd19a44a035cf7491841103
Ahmadzai Haseeb <68177125+ahmadzai2002@users.noreply.github.com>
2022-05-07 16:33:59 +0200

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,80 @@
+package blackjack;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Card {
+
+	public static void main(String[] args) {
+        String anotherCard, playAgain = "y", ctn = null;
+        int nextCard, card1, card2, dCard1, dCard2;
+        int cardTotal = 0, dTotal = 0;
+ 
+        Scanner keyboard = new Scanner(System.in);
+ 
+        Random random = new Random();
+ 
+        // Begin dealing the players first two cards
+ 
+        while ("y".equals(playAgain)) {
+            // dealers first two random cards
+            dCard1 = random.nextInt(10) + 1;
+            dCard2 = random.nextInt(10) + 1;
+ 
+            // players first two random cards and card total
+            card1 = random.nextInt(10) + 1;
+            card2 = random.nextInt(10) + 1;
+            cardTotal = card1 + card2;
+ 
+            // dealers two card total and display only one dealer card
+            dTotal = dCard1 + dCard2;
+            System.out.println("Dealer montre: " + dCard1);
+ 
+            // display players first two cards & card total
+            System.out.println("Premiere Carte: " + card1 + ", " + card2);
+            System.out.println("Totale: " + cardTotal);
+ 
+            System.out.println("Deuxieme Carte (y/n)?: ");
+            anotherCard = keyboard.nextLine();
+ 
+            while ("y".equals(anotherCard)) {
+                nextCard = random.nextInt(10) + 1;
+                cardTotal += nextCard;
+                System.out.println("Carte: " + nextCard);
+                System.out.println("Totale: " + cardTotal);
+ 
+                if (cardTotal > 21) {
+                    System.out.println("Perdu, Dealer a gagné");
+                    System.out.println("Voulez vous jouer encore? (y/n):");
+                    playAgain = keyboard.nextLine();
+                }
+ 
+                if (cardTotal < 21)
+                    System.out.println("Deuxieme Carte (y/n)?: ");
+                 
+                anotherCard = keyboard.nextLine();
+                 
+                if ("n".equals(anotherCard))
+                    System.out.println("Dealer avait: " + dTotal);
+                 
+                System.out.println("Tu avais: " + cardTotal);
+ 
+                while ("n".equals(anotherCard)) {
+                    if (dTotal < cardTotal && cardTotal < 21) {
+                        System.out.println("ouayy tu gagne!");
+                        System.out.println("Joue encore? (y/n): ");
+                        playAgain = keyboard.nextLine();
+                         
+                        if (playAgain.equalsIgnoreCase("y"))
+                            playAgain = "y";
+ 
+                        if (dTotal > cardTotal && dTotal < 21)
+                            System.out.println("Tu Perds!");
+                         
+                        playAgain = keyboard.nextLine();
+                    }
+                }
+            }
+        }
+    }
+}
